708e3138b44d8c6b4e68c59bea1ffff06ff34a80
natalie-abwoga <natalie.abwoga@strathmore.edu>
2025-06-20 09:08:09 +0300

    Update selection_sort.py

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,14 @@
+# TODO: Implement selection sort
 def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+    # Make a copy so we don't modify the original list
+    result = arr.copy()
+    n = len(result)
+
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if result[j] < result[min_index]:
+                min_index = j
+        result[i], result[min_index] = result[min_index], result[i]
+
+    return result
