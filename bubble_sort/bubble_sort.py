aeaf7c94ae5de6d15ed93bfadbcae3f0dbed3929
natalie-abwoga <natalie.abwoga@strathmore.edu>
2025-06-20 10:05:32 +0300

    Update bubble_sort.py

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,11 +1,10 @@
 # TODO: Implement bubble sort
 def bubble_sort(unsorted_list):
-    arr = unsorted_list.copy()
-    n = len(arr)
+    n = len(unsorted_list)
     for i in range(n):
         for j in range(0, n - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-    return arr
+            if unsorted_list[j] > unsorted_list[j + 1]:
+                unsorted_list[j], unsorted_list[j + 1] = unsorted_list[j + 1], unsorted_list[j]
+    return unsorted_list
 
 
